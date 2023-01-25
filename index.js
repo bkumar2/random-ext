b9729c92556b3d03c5eb3c9133146fc82e792f20
Bipul Kumar <bipul.kumar@gmail.com>
2023-01-25 14:13:46 +0530

    reformatted

--- index.js
+++ index.js
@@ -18,357 +18,339 @@
  */
 
 function _array(length, elementFunction, args) {
-  var array = [];
-  if (length != null) {
-    for (var i = 0; i < length; ++i) {
-      array[i] = elementFunction.apply(this, args);
+    var array = [];
+    if (length != null) {
+        for (var i = 0; i < length; ++i) {
+            array[i] = elementFunction.apply(this, args);
+        }
+    } else {
+        throw "length is required.";
     }
-  } else {
-    throw "length is required.";
-  }
-  return array;
+    return array;
 }
 
 function boolean() {
-  return Math.random() < 0.5;
+    return Math.random() < 0.5;
 }
 
 function booleanArray(length) {
-  return _array(length, boolean);
+    return _array(length, boolean);
 }
 
 function float(limit, min) {
-  if (limit != null) {
-    if (min != null) {
-      return (Math.random() * (limit - min)) + min;
+    if (limit != null) {
+        if (min != null) {
+            return Math.random() * (limit - min) + min;
+        } else {
+            return Math.random() * limit;
+        }
     } else {
-      return Math.random() * limit;
+        throw "max is required.";
     }
-  } else {
-    throw "max is required.";
-  }
 }
 
 function floatArray(length, limit, min) {
-  return _array(length, float, [limit, min]);
+    return _array(length, float, [limit, min]);
 }
 
 function integer(max, min) {
-  if (max != null) {
-    if (min != null) {
-      if (max < min) {
-        throw "max [" + max + "] is less than min [" + min + "]";
-      }
-      return Math.floor(Math.random() * (max - min + 1)) + min;
+    if (max != null) {
+        if (min != null) {
+            if (max < min) {
+                throw "max [" + max + "] is less than min [" + min + "]";
+            }
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        } else {
+            return Math.floor(Math.random() * (max + 1));
+        }
     } else {
-      return Math.floor(Math.random() * (max + 1));
+        throw "max is required.";
     }
-  } else {
-    throw "max is required.";
-  }
 }
 
 function integerArray(length, max, min) {
-  return _array(length, integer, [max, min]);
+    return _array(length, integer, [max, min]);
 }
 
 function _normalizeRanges(ranges) {
-  if (randomExt.DEBUG) {
-    console.log("Normalizing ranges:", ranges);
-  }
-  ranges.sort(function(a, b) {
-    return a[0] - b[0];
-  });
-  if (randomExt.DEBUG) {
-    console.log("sorted ranges:", ranges);
-  }
-  for (var i = 0; i < ranges.length - 1; ++i) {
-    for (var j = i + 1; j < ranges.length; ++j) {
-      // remove right contained
-      if (ranges[i][1] >= ranges[j][1]) {
-        ranges.splice(j, 1);
-        j--;
-      } else
-        // fix overlap
-        if (ranges[i][1] >= ranges[j][0]) {
-          ranges[j][0] = ranges[i][1] + 1;
-        }
-      if (randomExt.DEBUG) {
-        console.log("iteration (" + i + "," + j + "):", ranges);
-      }
+    if (randomExt.DEBUG) {
+        console.log("Normalizing ranges:", ranges);
     }
-  }
-  if (randomExt.DEBUG) {
-    console.log("Normalized ranges:", ranges);
-  }
+    ranges.sort(function (a, b) {
+        return a[0] - b[0];
+    });
+    if (randomExt.DEBUG) {
+        console.log("sorted ranges:", ranges);
+    }
+    for (var i = 0; i < ranges.length - 1; ++i) {
+        for (var j = i + 1; j < ranges.length; ++j) {
+            // remove right contained
+            if (ranges[i][1] >= ranges[j][1]) {
+                ranges.splice(j, 1);
+                j--;
+            }
+            // fix overlap
+            else if (ranges[i][1] >= ranges[j][0]) {
+                ranges[j][0] = ranges[i][1] + 1;
+            }
+            if (randomExt.DEBUG) {
+                console.log("iteration (" + i + "," + j + "):", ranges);
+            }
+        }
+    }
+    if (randomExt.DEBUG) {
+        console.log("Normalized ranges:", ranges);
+    }
 }
 
 function _integerFromRanges(ranges) {
-  _normalizeRanges(ranges);
-  if (ranges != null) {
-    var span = 0;
-    for (var i = 0; i < ranges.length; ++i) {
-      span += (ranges[i][1] - ranges[i][0] + 1);
+    _normalizeRanges(ranges);
+    if (ranges != null) {
+        var span = 0;
+        for (var i = 0; i < ranges.length; ++i) {
+            span += ranges[i][1] - ranges[i][0] + 1;
+        }
+        var randomNumber = Math.floor(Math.random() * span);
+        for (var i = 0; i < ranges.length; ++i) {
+            randomNumber += ranges[i][0];
+            if (randomNumber <= ranges[i][1]) {
+                break;
+            } else {
+                randomNumber -= ranges[i][1] + 1;
+            }
+        }
+        return randomNumber;
+    } else {
+        throw "ranges is required.";
     }
-    var randomNumber = Math.floor(Math.random() * span);
-    for (var i = 0; i < ranges.length; ++i) {
-      randomNumber += ranges[i][0];
-      if (randomNumber <= ranges[i][1]) {
-        break;
-      } else {
-        randomNumber -= (ranges[i][1] + 1);
-      }
-    }
-    return randomNumber;
-  } else {
-    throw "ranges is required.";
-  }
 }
 
 function _integerArrayFromRanges(length, ranges) {
-  var numberArray = [];
-  if (length != null && ranges != null) {
-    for (var i = 0; i < length; ++i) {
-      numberArray[i] = _integerFromRanges(ranges);
+    var numberArray = [];
+    if (length != null && ranges != null) {
+        for (var i = 0; i < length; ++i) {
+            numberArray[i] = _integerFromRanges(ranges);
+        }
+    } else {
+        throw "length and ranges is required.";
     }
-  } else {
-    throw "length and ranges is required.";
-  }
-  return numberArray;
+    return numberArray;
 }
 
 function _stringFromRanges(maxLength, minLength, ranges) {
-  var dString = "";
-  var length = integer(maxLength, minLength);
-  var unicodeNumbers = _integerArrayFromRanges(length, ranges);
-  dString = String.fromCharCode.apply(this, unicodeNumbers);
-  return dString;
+    var dString = "";
+    var length = integer(maxLength, minLength);
+    var unicodeNumbers = _integerArrayFromRanges(length, ranges);
+    dString = String.fromCharCode.apply(this, unicodeNumbers);
+    return dString;
 }
 
 function date(endDate, startDate) {
-  if (endDate == null) {
-    throw "end date is required.";
-  }
-  var endDateTime = endDate.getTime();
-  var startDateTime = startDate != null ? startDate.getTime() : 0;
-  return new Date(integer(endDateTime, startDateTime));
+    if (endDate == null) {
+        throw "end date is required.";
+    }
+    var endDateTime = endDate.getTime();
+    var startDateTime = startDate != null ? startDate.getTime() : 0;
+    return new Date(integer(endDateTime, startDateTime));
 }
 
 function dateArray(length, endDate, startDate) {
-  return _array(length, date, [endDate, startDate]);
+    return _array(length, date, [endDate, startDate]);
 }
 
 function string(maxLength, minLength) {
-  if (randomExt.DEBUG) {
-    console.log("string maxLength:", maxLength, " minLength:",
-      minLength);
-  }
-  return _stringFromRanges(maxLength, minLength, [
-    [32, 126]
-  ]);
+    if (randomExt.DEBUG) {
+        console.log("string maxLength:", maxLength, " minLength:", minLength);
+    }
+    return _stringFromRanges(maxLength, minLength, [[32, 126]]);
 }
 
 function stringArray(arrayLength, stringMaxLength, stringMinLength) {
-  return _array(arrayLength, string, [stringMaxLength,
-    stringMinLength
-  ]);
+    return _array(arrayLength, string, [stringMaxLength, stringMinLength]);
 }
 
 function restrictedString(content, maxLength, minLength) {
-  var ranges = [];
-  for (var i = 0; i < content.length; ++i) {
-    var contentType = content[i];
-    switch (contentType) {
-      case randomExt.CHAR_TYPE.SPECIAL:
-        ranges = ranges.concat([
-          [33, 47],
-          [58, 64],
-          [91, 96],
-          [123, 126]
-        ]);
-        break;
-      case randomExt.CHAR_TYPE.SPACE:
-        ranges = ranges.concat([
-          [32, 32]
-        ]);
-        break;
-      case randomExt.CHAR_TYPE.NUMERIC:
-        ranges = ranges.concat([
-          [48, 57]
-        ]);
-        break;
-      case randomExt.CHAR_TYPE.UPPERCASE:
-        ranges = ranges.concat([
-          [65, 90]
-        ]);
-        break;
-      case randomExt.CHAR_TYPE.LOWERCASE:
-        ranges = ranges.concat([
-          [97, 122]
-        ]);
-        break;
-      case randomExt.CHAR_TYPE.HEX:
-        ranges = ranges.concat([
-          [48, 57],
-          [97, 102]
-        ]);
-        break;
-      default:
-        if (typeof contentType === "string") {
-          for (var j = 0; j < contentType.length; ++j) {
-            ranges = ranges.concat([
-              [contentType.charCodeAt(j),
-                contentType.charCodeAt(j)
-              ]
-            ]);
-          }
+    var ranges = [];
+    for (var i = 0; i < content.length; ++i) {
+        var contentType = content[i];
+        switch (contentType) {
+            case randomExt.CHAR_TYPE.SPECIAL:
+                ranges = ranges.concat([
+                    [33, 47],
+                    [58, 64],
+                    [91, 96],
+                    [123, 126],
+                ]);
+                break;
+            case randomExt.CHAR_TYPE.SPACE:
+                ranges = ranges.concat([[32, 32]]);
+                break;
+            case randomExt.CHAR_TYPE.NUMERIC:
+                ranges = ranges.concat([[48, 57]]);
+                break;
+            case randomExt.CHAR_TYPE.UPPERCASE:
+                ranges = ranges.concat([[65, 90]]);
+                break;
+            case randomExt.CHAR_TYPE.LOWERCASE:
+                ranges = ranges.concat([[97, 122]]);
+                break;
+            case randomExt.CHAR_TYPE.HEX:
+                ranges = ranges.concat([
+                    [48, 57],
+                    [97, 102],
+                ]);
+                break;
+            default:
+                if (typeof contentType === "string") {
+                    for (var j = 0; j < contentType.length; ++j) {
+                        ranges = ranges.concat([
+                            [contentType.charCodeAt(j), contentType.charCodeAt(j)],
+                        ]);
+                    }
+                }
         }
     }
-  }
-  return _stringFromRanges(maxLength, minLength, ranges);
+    return _stringFromRanges(maxLength, minLength, ranges);
 }
 
-function restrictedStringArray(arrayLength, content, stringMaxLength,
-  stringMinLength) {
-  return _array(arrayLength, restrictedString, [content,
-    stringMaxLength, stringMinLength
-  ]);
+function restrictedStringArray(arrayLength, content, stringMaxLength, stringMinLength) {
+    return _array(arrayLength, restrictedString, [content, stringMaxLength, stringMinLength]);
 }
 
 function _fromDescriptor(randomDescriptor) {
-  var randomValue = null;
-  if (randomDescriptor == null || !randomDescriptor.shift ||
-    randomDescriptor.length <= 0 ||
-    typeof randomDescriptor[0] !== "function") {
-    randomValue = randomDescriptor;
-  } else {
-    var randomFunction = randomDescriptor[0];
-    if (randomDescriptor.length > 1) {
-      var propertyValueArgs = randomDescriptor.slice(1,
-        randomDescriptor.length);
-      randomValue = randomFunction.apply(this, propertyValueArgs);
+    var randomValue = null;
+    if (
+        randomDescriptor == null ||
+        !randomDescriptor.shift ||
+        randomDescriptor.length <= 0 ||
+        typeof randomDescriptor[0] !== "function"
+    ) {
+        randomValue = randomDescriptor;
     } else {
-      randomValue = randomFunction();
+        var randomFunction = randomDescriptor[0];
+        if (randomDescriptor.length > 1) {
+            var propertyValueArgs = randomDescriptor.slice(1, randomDescriptor.length);
+            randomValue = randomFunction.apply(this, propertyValueArgs);
+        } else {
+            randomValue = randomFunction();
+        }
     }
-  }
-  return randomValue;
+    return randomValue;
 }
 
 function object(template) {
-  if (randomExt.DEBUG) {
-    console.log("object template:", template);
-  }
-  var newObject = {};
-  var properties = Object.getOwnPropertyNames(template);
-  for (var i = 0; i < properties.length; ++i) {
-    var property = properties[i];
-    var randomDescriptor = template[property];
-    newObject[property] = _fromDescriptor(randomDescriptor);
-  }
-  return newObject;
+    if (randomExt.DEBUG) {
+        console.log("object template:", template);
+    }
+    var newObject = {};
+    var properties = Object.getOwnPropertyNames(template);
+    for (var i = 0; i < properties.length; ++i) {
+        var property = properties[i];
+        var randomDescriptor = template[property];
+        newObject[property] = _fromDescriptor(randomDescriptor);
+    }
+    return newObject;
 }
 
 function objectArray(length, template) {
-  return _array(length, object, [template]);
+    return _array(length, object, [template]);
 }
 
 function stringPattern(pattern, variableDefinition) {
-  var stringPattern = pattern;
-  var properties = Object.getOwnPropertyNames(variableDefinition);
-  var replacedStringArray = new Array();
-  for (var i = 0; i < stringPattern.length; ++i) {
-    if (variableDefinition.hasOwnProperty(stringPattern[i])) {
-      replacedStringArray[i] = _fromDescriptor(variableDefinition[stringPattern[i]]);
-    } else {
-      replacedStringArray[i] = stringPattern[i];
+    var stringPattern = pattern;
+    var properties = Object.getOwnPropertyNames(variableDefinition);
+    var replacedStringArray = new Array();
+    for (var i = 0; i < stringPattern.length; ++i) {
+        if (variableDefinition.hasOwnProperty(stringPattern[i])) {
+            replacedStringArray[i] = _fromDescriptor(variableDefinition[stringPattern[i]]);
+        } else {
+            replacedStringArray[i] = stringPattern[i];
+        }
     }
-  }
-  return replacedStringArray.join("");
+    return replacedStringArray.join("");
 }
 
 function stringPatternArray(length, pattern, variableDefinition) {
-  return _array(length, stringPattern, [pattern,
-    variableDefinition
-  ]);
+    return _array(length, stringPattern, [pattern, variableDefinition]);
 }
 
 function pick(array) {
-  if (array == null) {
-    throw "input array is null or undefined.";
-  }
-  return array[integer(array.length - 1)];
+    if (array == null) {
+        throw "input array is null or undefined.";
+    }
+    return array[integer(array.length - 1)];
 }
 
 function shuffle(array) {
-  if (array == null) {
-    throw "input array is null or undefined.";
-  }
-  for (var i = 0; i < array.length; ++i) {
-    var randomIndex = integer(array.length - 1);
-    var temp = array[randomIndex];
-    array[randomIndex] = array[i];
-    array[i] = temp;
-  }
+    if (array == null) {
+        throw "input array is null or undefined.";
+    }
+    for (var i = 0; i < array.length; ++i) {
+        var randomIndex = integer(array.length - 1);
+        var temp = array[randomIndex];
+        array[randomIndex] = array[i];
+        array[i] = temp;
+    }
 }
 
 function subArray(array, length) {
-  // validation
-  if (array == null) {
-    throw "input array is null or undefined.";
-  } else if (length == null) {
-    throw "input length is null or undefined.";
-  } else if (array.length < length) {
-    throw "input length [" + length + "] must not exceed array length [" + array.length + "]";
-  }
-  // logic
-  var copiedArray = array.slice();
-  shuffle(copiedArray);
-  return copiedArray.slice(0, length);
+    // validation
+    if (array == null) {
+        throw "input array is null or undefined.";
+    } else if (length == null) {
+        throw "input length is null or undefined.";
+    } else if (array.length < length) {
+        throw "input length [" + length + "] must not exceed array length [" + array.length + "]";
+    }
+    // logic
+    var copiedArray = array.slice();
+    shuffle(copiedArray);
+    return copiedArray.slice(0, length);
 }
 
 function color() {
-  return '#'.concat((Math.random() * 0xFFFFFF << 0).toString(16));
+    return "#".concat(((Math.random() * 0xffffff) << 0).toString(16));
 }
 
 function guid() {
-  return randomExt.stringPattern("xxxxxxxx-xxxx-yxxx-zxxx-xxxxxxxxxxxx",{
-    x: [randomExt.restrictedString, [randomExt.CHAR_TYPE.HEX], 1, 1],
-    y: [randomExt.restrictedString, ["12345"], 1, 1],
-    z: [randomExt.restrictedString, ["89ab"], 1, 1]
-  })
+    return randomExt.stringPattern("xxxxxxxx-xxxx-yxxx-zxxx-xxxxxxxxxxxx", {
+        x: [randomExt.restrictedString, [randomExt.CHAR_TYPE.HEX], 1, 1],
+        y: [randomExt.restrictedString, ["12345"], 1, 1],
+        z: [randomExt.restrictedString, ["89ab"], 1, 1],
+    });
 }
 
 var randomExt = {
-  boolean: boolean,
-  booleanArray: booleanArray,
-  integer: integer,
-  integerArray: integerArray,
-  float: float,
-  floatArray: floatArray,
-  date: date,
-  dateArray: dateArray,
-  string: string,
-  stringArray: stringArray,
-  restrictedString: restrictedString,
-  restrictedStringArray: restrictedStringArray,
-  object: object,
-  objectArray: objectArray,
-  stringPattern: stringPattern,
-  stringPatternArray: stringPatternArray,
-  pick: pick,
-  shuffle: shuffle,
-  subArray: subArray,
-  color: color,
-  guid: guid,
-  CHAR_TYPE: {
-    LOWERCASE: 0,
-    UPPERCASE: 1,
-    NUMERIC: 2,
-    SPECIAL: 3,
-    SPACE: 4,
-    HEX: 5
-  },
-  DEBUG: false
+    boolean: boolean,
+    booleanArray: booleanArray,
+    integer: integer,
+    integerArray: integerArray,
+    float: float,
+    floatArray: floatArray,
+    date: date,
+    dateArray: dateArray,
+    string: string,
+    stringArray: stringArray,
+    restrictedString: restrictedString,
+    restrictedStringArray: restrictedStringArray,
+    object: object,
+    objectArray: objectArray,
+    stringPattern: stringPattern,
+    stringPatternArray: stringPatternArray,
+    pick: pick,
+    shuffle: shuffle,
+    subArray: subArray,
+    color: color,
+    guid: guid,
+    CHAR_TYPE: {
+        LOWERCASE: 0,
+        UPPERCASE: 1,
+        NUMERIC: 2,
+        SPECIAL: 3,
+        SPACE: 4,
+        HEX: 5,
+    },
+    DEBUG: false,
 };
 
 module.exports = randomExt;
--- test.js
+++ test.js
@@ -6,13 +6,13 @@
  * Released under the Apache License 2.0
  */
 var randomExt = require("./index.js");
-var expect = require('chai').expect;
+var expect = require("chai").expect;
 
 var randomBoolean = randomExt.boolean();
-expect(randomBoolean).not.to.be.an('undefined');
+expect(randomBoolean).not.to.be.an("undefined");
 
 var randomBooleanArray = randomExt.booleanArray(10);
-expect(randomBooleanArray).not.to.be.an('undefined');
+expect(randomBooleanArray).not.to.be.an("undefined");
 expect(randomBooleanArray.length).to.equal(10);
 
 var integer = randomExt.integer(10, 2);
@@ -22,8 +22,8 @@ expect(integer).to.be.above(1);
 var integerArray = randomExt.integerArray(10, 10, 2);
 expect(integerArray.length).to.equal(10);
 for (var i = 0; i < integerArray.length; i++) {
-  expect(integer).to.be.below(11);
-  expect(integer).to.be.above(1);
+    expect(integer).to.be.below(11);
+    expect(integer).to.be.above(1);
 }
 
 var randomFloat = randomExt.float(10.1, 9.9);
@@ -33,8 +33,8 @@ expect(randomFloat).to.be.above(9.89);
 var randomFloatArray = randomExt.floatArray(3, 100.23423, 0.4);
 expect(randomFloatArray.length).to.equal(3);
 for (var i = 0; i < randomFloatArray.length; i++) {
-  expect(randomFloatArray[i]).to.be.below(100.23423);
-  expect(randomFloatArray[i]).to.be.above(0.4);
+    expect(randomFloatArray[i]).to.be.below(100.23423);
+    expect(randomFloatArray[i]).to.be.above(0.4);
 }
 
 var randomDate = randomExt.date(new Date());
@@ -43,7 +43,7 @@ expect(randomDate).to.be.below(new Date());
 var randomDateArray = randomExt.dateArray(3, new Date());
 expect(randomDateArray.length).to.equal(3);
 for (var i = 0; i < randomDateArray.length; i++) {
-  expect(randomDateArray[i]).to.be.below(new Date());
+    expect(randomDateArray[i]).to.be.below(new Date());
 }
 
 var randomString = randomExt.string(10, 5);
@@ -54,39 +54,42 @@ expect(randomString.length).to.be.above(4);
 var randomStringArray = randomExt.stringArray(6, 4, 2);
 expect(randomStringArray.length).to.equal(6);
 for (var i = 0; i < randomStringArray.length; i++) {
-  expect(randomStringArray[i].length).to.be.below(5);
-  expect(randomStringArray[i].length).to.be.above(1);
+    expect(randomStringArray[i].length).to.be.below(5);
+    expect(randomStringArray[i].length).to.be.above(1);
 }
 
 var randomSnakeCaseVariableName = randomExt.restrictedString(
-  [randomExt.CHAR_TYPE.LOWERCASE, "_"], 20, 10);
+    [randomExt.CHAR_TYPE.LOWERCASE, "_"],
+    20,
+    10
+);
 expect(typeof randomSnakeCaseVariableName).to.be.a("string");
 expect(randomSnakeCaseVariableName.length).to.be.below(21);
 expect(randomSnakeCaseVariableName.length).to.be.above(9);
 
 var randomObject = randomExt.object({
-  name: [randomExt.restrictedString, [randomExt.CHAR_TYPE.LOWERCASE], 10, 5],
-  gender: [randomExt.pick, ["Male", "Female"]],
-  type: "Customer",
-  age: [randomExt.integer, 100]
+    name: [randomExt.restrictedString, [randomExt.CHAR_TYPE.LOWERCASE], 10, 5],
+    gender: [randomExt.pick, ["Male", "Female"]],
+    type: "Customer",
+    age: [randomExt.integer, 100],
 });
 expect(typeof randomObject.name).to.be.a("string");
 
 var randomObjectArray = randomExt.objectArray(4, {
-  name: [randomExt.string, 10, 5],
-  age: [randomExt.integer, 100]
+    name: [randomExt.string, 10, 5],
+    age: [randomExt.integer, 100],
 });
 expect(randomObjectArray.length).to.equal(4);
 
 var randomStringPattern = randomExt.stringPattern("x@x.com", {
-  x: [randomExt.restrictedString, [randomExt.CHAR_TYPE.LOWERCASE], 10, 6]
+    x: [randomExt.restrictedString, [randomExt.CHAR_TYPE.LOWERCASE], 10, 6],
 });
 expect(randomStringPattern.indexOf("@")).to.be.above(5);
 expect(randomStringPattern.indexOf(".com")).to.be.above(12);
 
 var randomStringPatternArray = randomExt.stringPatternArray(5, "Xy Xy", {
-  X: [randomExt.restrictedString, [randomExt.CHAR_TYPE.UPPERCASE], 1, 1],
-  y: [randomExt.restrictedString, [randomExt.CHAR_TYPE.LOWERCASE], 20]
+    X: [randomExt.restrictedString, [randomExt.CHAR_TYPE.UPPERCASE], 1, 1],
+    y: [randomExt.restrictedString, [randomExt.CHAR_TYPE.LOWERCASE], 20],
 });
 expect(randomStringPatternArray.length).to.equal(5);
 
